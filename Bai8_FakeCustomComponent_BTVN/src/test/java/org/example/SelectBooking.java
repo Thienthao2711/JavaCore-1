be90961ccb15c18ebc2bbbc087f4f56914736ece
Thienthao2711 <71122358+Thienthao2711@users.noreply.github.com>
2020-10-21 20:01:29 +0700

    t

--- Bai8_FakeCustomComponent_BTVN/src/test/java/org/example/SelectBooking.java
+++ Bai8_FakeCustomComponent_BTVN/src/test/java/org/example/SelectBooking.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,76 +14,65 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+
+public class SelectBooking {
     WebDriver driver;
 
     @Before
-    public void inIt() {
+    public void startChrome() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         this.driver = new ChromeDriver();
+        this.driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         this.driver.get("https://www.booking.com/");
+        this.driver.manage().window().maximize();
 
     }
 
-    @Test
-    public void checkSearchBoxDestination() {
-        WebElement searchDestination = driver.findElement(By.cssSelector("input[type=search]"));
-        searchDestination.click();
-        String input = "Hanoi";
-        String expectedResultContain = input.toLowerCase();
-        searchDestination.sendKeys("hanoi");
-        WebDriverWait webDriverWait = new WebDriverWait(this.driver, 15);
-        webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("ul[role=listbox]"))));
-        List<WebElement> elements = driver.findElements(By.cssSelector("li[role=option]"));
-        for (int i = 0; i < elements.size(); i++) {
-            System.out.println(elements.get(i).getText());
-            String stringOption = elements.get(i).getText().replaceAll("\\s+", "");//xoa tat ca cac ky tu khong nhin thay duoc
 
-            String lowerCaseActual = stringOption.toLowerCase();
-            System.out.println(lowerCaseActual);
-            Assert.assertTrue(lowerCaseActual.contains(expectedResultContain));
+    @Test
+    public void searchTextbox() {
+        String Searchvaule = "HA NOI";
+        String ChuyendoiSearchvaule = Searchvaule.toUpperCase().replaceAll("\\s+", " ");
+
+
+        WebElement searchTxt = driver.findElement(By.cssSelector("input[type=search]"));
+        searchTxt.sendKeys("HA NOI");
+        WebDriverWait Wait = new WebDriverWait(this.driver, 15);
+        Wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("ul.-visible"))));
+        List<WebElement> list = driver.findElements(By.cssSelector("ul.-visible"));
+        for (int i = 0; i < list.size(); i++) {
+            String Chuanchuoielemt = list.get(i).getText().replaceAll("\\s+", " ") /*xóa đi khoảng trắng thừa */;
+            String InHoaKyTu = Chuanchuoielemt.toUpperCase();
+            Assert.assertTrue(InHoaKyTu.contains(ChuyendoiSearchvaule));
 
         }
-    }
 
+
+        }
     @Test
-    public void checkSearchCheckInCheckOut() throws InterruptedException {
+    public void Bookingday(){
         WebElement checkInCheckOut = driver.findElement(By.cssSelector("span[class=\"sb-date-field__icon sb-date-field__icon-btn bk-svg-wrapper calendar-restructure-sb\"]"));
         checkInCheckOut.click();
         WebDriverWait webDriverWait = new WebDriverWait(this.driver, 15);
         webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("div[class=\"bui-calendar__month\"]"))));
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter Format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime checkIn = LocalDateTime.now().plusDays(1);
         LocalDateTime checkOut = LocalDateTime.now().plusDays(3);
-        String elementCheckIn = "td[data-date=\"" + dtf.format(checkIn) + "\"]";
-        String elementCheckOut = "td[data-date=\"" + dtf.format(checkOut) + "\"]";
+        String elementCheckIn = "td[data-date=\"" + Format.format(checkIn) + "\"]";
+        String elementCheckOut = "td[data-date=\"" + Format.format(checkOut) + "\"]";
         WebElement checkInItem = driver.findElement(By.cssSelector(elementCheckIn));
         checkInItem.click();
         WebElement checkOutItem = driver.findElement(By.cssSelector(elementCheckOut));
         checkOutItem.click();
-        //Cho cho toi khi khong tim thay chuoi check out (co nghia search da duoc chon)
-        Thread.sleep(1000);
-        //Hoi anh Khanh co cach nao khac sleep
-        //So sanh voi ExpectedResult
-        //Ngay hien tai 15
-        String expectedResultCheckIn = "Oct 16";
-        String expectedResultCheckOut = "Oct 18";
-        WebElement webElementCheckOut = driver.findElement(By.cssSelector("div[data-placeholder=\"Check-out\"]"));
-        WebElement webElementCheckIn = driver.findElement(By.cssSelector("div[data-placeholder=\"Check-in\"]"));
-        Assert.assertTrue(webElementCheckIn.getText().trim().contains(expectedResultCheckIn));
-        Assert.assertTrue(webElementCheckOut.getText().trim().contains(expectedResultCheckOut));
+
+
     }
 
     @Test
-    public void checkInputAdultChildRoom() throws InterruptedException {
+    public void  persionChoose(){
         WebElement componentAdultChildRoom = driver.findElement(By.cssSelector("span.xp__guests__count"));
         componentAdultChildRoom.click();
         //Doi den khi mo Popup
@@ -113,27 +100,12 @@ public class AppTest {
             buttonIncreaseRoom.click();
         }
 
-        Thread.sleep(1000);
-        String actualAdult = driver.findElement(By.cssSelector("span[data-adults-count]")).getText();
-        String actualChild = driver.findElement(By.cssSelector("span[data-children-count]")).getText();
-        String actualRoom = driver.findElement(By.cssSelector("span[data-room-count]")).getText();
-        String expectedAdult = "5";
-        String expectedChild = "1";
-        String expectedRoom = "3";
-        System.out.println(actualAdult);
-        System.out.println(actualChild);
-        System.out.println(actualRoom);
-        Assert.assertTrue(actualAdult.contains(expectedAdult));
-        Assert.assertTrue(actualChild.contains(expectedChild));
-        Assert.assertTrue(actualRoom.contains(expectedRoom));
-
-
     }
+
 
 
     @After
     public void afterTest() {
         driver.quit();
     }
-
 }
